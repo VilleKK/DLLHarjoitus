3b8ce91a9e2f6f89a65c0ec24c589a675e7cef9b
villekk <ville.kurkela@seamk.fi>
2015-01-22 18:47:47 +0200

    eka versio

---
+++ FilteringMethods/FilterFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteringMethods
+{
+    public class FilterFactory
+    {
+        public enum FilterTypes
+        {
+            Median, Average
+        }
+        public static Filter CreateFilter(FilterTypes type, int length)
+        {
+            Filter f = null;
+            switch (type)
+            {
+                case FilterTypes.Average:
+                    f = new AverageFilter(length);
+                    break;
+                case FilterTypes.Median:
+                    f = new MedianFilter(length);
+                    break;
+            }
+            return f;
+        }
+    }
+}
